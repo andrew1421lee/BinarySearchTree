a32bdd205f374c5f40076d03a590d7eeb835c999
David Dvorkin <davidfreddvorkin@gmail.com>
2016-11-15 17:13:15 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,6 +1,37 @@
 #include "BinarySearchTree.h"
+
 using namespace std;
 
-int main(){
-    
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+{
+    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+    result->tv_sec = diff / 1000000;
+    result->tv_usec = diff % 1000000;
+
+    return (diff<0);
 }
+
+void timeval_print(char *str, struct timeval *tv)
+{
+    printf("%s %ld sec, %06ld micro sec\n", str, tv->tv_sec, tv->tv_usec);
+}
+
+int main(){
+        struct timeval tvDIFF, tvStart, tvEnd;
+
+        /*Timer:*/
+
+        gettimeofday(&tvStart, NULL);
+
+        /*PUT FUNCTION HERE*/
+
+        gettimeofday(&tvEnd, NULL);
+
+        timeval_subtract(&tvDIFF, &tvEnd, &tvStart);
+        timeval_print ("This took: ", &tvDIFF);
+       /* char str[] = {'T', 'h', 'i', 's', ' ', 't', 'o', 'o', 'k', ':', ' '};
+        timeval_print(str, &tvDIFF);*/
+
+        return 0;
+}
+
