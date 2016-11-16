b2b58ae280331a6997ea33094c6e8d1fd3658ecf
James Yao <jamesy3608@gmail.com>
2016-11-16 05:50:04 -0500

    Add files via upload

--- Driver.cpp
+++ Driver.cpp
@@ -22,38 +22,46 @@ int main()
 	// Testing with integers
 	{
 		cout << "Testing 1000 integers:" << endl << endl;
-		// Timer
+
+		// Start timer
 		gettimeofday(&tvStart, NULL);
 
-		// PUT FUNCTION HERE
+		// Make tree
 		BST<int>* tree = new BST<int>(50000);
 		for (int i = 0; i < 1000; i++) tree->insert(rand() % 100000);
+
 		tree->display();
 
+		// Stop timer
 		gettimeofday(&tvEnd, NULL);
 
 		timeval_subtract(&tvDIFF, &tvEnd, &tvStart);
 		cout << endl;
 		timeval_print ("This took: ", &tvDIFF);
+
 		cout << "--------------------------------" << endl;
 	}
 
 	// Testing with strings
 	{
 		cout << "Testing 1000 strings:" << endl << endl;
-		// Timer
+
+		// Start timer
 		gettimeofday(&tvStart, NULL);
 
-		// PUT FUNCTION HERE
+		// Make tree
 		BST<string>* tree = new BST<string>("hello");
 		for (int i = 0; i < 1000; i++) tree->insert(rand_str(rand() % 10));
+
 		tree->display();
 
+		// Stop timer
 		gettimeofday(&tvEnd, NULL);
 
 		timeval_subtract(&tvDIFF, &tvEnd, &tvStart);
 		cout << endl;
 		timeval_print ("This took: ", &tvDIFF);
+
 		cout << "--------------------------------" << endl;
 	}
 
@@ -66,7 +74,6 @@ int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval 
     long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
     result->tv_sec = diff / 1000000;
     result->tv_usec = diff % 1000000;
-
     return (diff<0);
 }
 
@@ -74,7 +81,8 @@ int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval 
 void timeval_print(string str, struct timeval *tv)
 {
 	cout << str;
-    printf("%ld sec, %06ld micro sec\n", tv->tv_sec, tv->tv_usec);
+    printf("%ld sec, %06ld micro sec", tv->tv_sec, tv->tv_usec);
+	cout << endl;
 }
 
 // Generates a random string with length len and only lower case letters
